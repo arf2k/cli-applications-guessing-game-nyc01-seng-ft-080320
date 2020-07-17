c9e9c8badce1a4423a0bad39185fbd578d599a4a
Alex Foreman <foreman.alexander@gmail.com>
2020-07-17 19:03:48 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -8,11 +8,11 @@ def prompt_user
   puts "Please enter a number between 1 and 6"
 end
 
-def get_input(input)
-  input = gets.chomp
-end
+# def get_input(input)
+#   input = gets.chomp
+# end
 
-def compare_numbers(random, user)
+# def compare_numbers(random, user)
   # generate_number(random) = random 
   # user = get_input(input) 
   # if random == user 
