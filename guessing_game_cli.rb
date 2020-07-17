570226b14854d494268cee59eb2ef81818777e88
Alex Foreman <foreman.alexander@gmail.com>
2020-07-17 19:15:33 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -7,6 +7,8 @@ def run_guessing_game
     puts "You guessed the correct number!"
   elsif input == "exit"
     puts "Goodbye!"
-  elsif input 
+  else
+    puts "Sorry! The computer guessed #{random}."
+  end
 
 end 
